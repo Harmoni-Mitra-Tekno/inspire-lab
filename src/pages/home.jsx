21f861065a3d9ecc55a9499e9a72caa1e1630e52
Ananda Dana Pratama <ananda.pratama.ecomindo@external.bankmas.co.id>
2025-10-27 14:12:49 +0700

    add animations

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,35 @@
 import Aurora from "../components/animations/aurora";
+import ChromaGrid from "../components/animations/chromaGrid";
 import ScrollReveal from "../components/animations/scrollReveal";
+import SpotlightCard from "../components/animations/spotlightCard";
 import TextType from "../components/animations/textType";
 import Threads from "../components/animations/threads";
 import { motion } from "framer-motion";
+import fadhlanProfile from "../assets/fadhlan-profile.jpg"
+
+const items = [
+  {
+    image: "https://i.pravatar.cc/300?img=1",
+    title: "Griffani Megiyanto Rahmatullah",
+    subtitle: "Frontend Developer",
+    gradient: "linear-gradient(145deg, #0926f4, #0a0a0a)",
+    url: "https://github.com/sarahjohnson"
+  },
+  {
+    image: {fadhlanProfile},
+    title: "Muhammad Yusuf Fadhlan",
+    subtitle: "Researcher",
+    gradient: "linear-gradient(180deg, #0926f4, #0a0a0a)",
+    url: "https://www.linkedin.com/in/griffani-megiyanto-rahmatullah-258224145/"
+  },
+  {
+    image: "https://i.pravatar.cc/300?img=2",
+    title: "Nilla Novita Sari",
+    subtitle: "Researcher",
+    gradient: "linear-gradient(180deg, #0926f4, #0a0a0a)",
+    url: "https://www.linkedin.com/in/nila-novita-sari-573b28133"
+  }
+];
 
 function Home() {
     const headline = "Discover the Inspirational Research at Intelligent Signal Processing Research Laboratory";
@@ -52,8 +79,37 @@ function Home() {
                     baseRotation={10}
                     blurStrength={10}
                 >
-                    We are the core engine for advanced information technology. ISPL centers on exploiting novel signal processing theory, advanced statistical modeling, and machine intelligence to solve critical challenges in video big data analytics, speech understanding, and complex human-machine interaction.
+                    We are the core engine for advanced information technology. INSPIRE centers on exploiting novel signal processing theory, advanced statistical modeling, and machine intelligence to solve critical challenges in video big data analytics, speech understanding, and complex human-machine interaction.
                 </ScrollReveal>
+            </div>
+            <div className="flex flex-row p-20 gap-5">
+                <SpotlightCard className="w-1/3" spotlightColor="#0926f4">
+                    <div className="flex flex-col gap-2">
+                        <p className="text-3xl font-semibold">Advances in Audio, Visual, and Language Processing</p>
+                        <p>Our research investigates image and video processing, as well as acoustic and speech processing. We also work on speech recognition and understanding, video big data analytics, motion tracking, and natural language understanding and generation.</p>
+                    </div>
+                </SpotlightCard>
+                <SpotlightCard className="w-1/3" spotlightColor="#0926f4">
+                    <div className="flex flex-col gap-2">
+                        <p className="text-3xl font-semibold">Core Algorithms for Intelligent Systems</p>
+                        <p>The INSPIRE team develops the core algorithms that power intelligent systems. We use a multi-faceted approach, combining advanced statistical modeling, cutting-edge signal processing theory, and machine intelligence techniques to meet the demands of a rapidly evolving information-technology landscape. Our extensive research is reflected in our many journal publications across diverse fields, including image and speech signal processing, data fusion, pattern recognition, and intelligent systems.</p>
+                    </div>
+                </SpotlightCard>
+                <SpotlightCard className="w-1/3" spotlightColor="#0926f4">
+                    <div className="flex flex-col gap-2">
+                        <p className="text-3xl font-semibold">Building the Next Generation of Intelligent Surveillance</p>
+                        <p>Our vision is to develop algorithms that enable seamless human-machine interaction and powerful video big data analytics. We're creating intelligent surveillance systems by integrating diverse technologies like image, acoustic, and speech processing, as well as motion tracking and natural language understanding and generation.</p>
+                    </div>
+                </SpotlightCard>
+            </div>
+            <div className="bg-[#0a0a0a]">
+                <ChromaGrid
+                    items={items}
+                    radius={300}
+                    damping={0.45}
+                    fadeOut={0.6}
+                    ease="power3.out"
+                />
             </div>
         </div>
         
